20e8fb852e5de773fa87e72e8825f7252138ab07
ABustos2606 <info@panaceaediciones.com.ar>
2022-08-04 20:32:30 -0300

    JS operations working

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let value1 = "";
 let value2 = 0;
-let operator;
+let operator = "";
 let operatorIsActive = false;
 let calcButtons = document.querySelectorAll('button');
 
@@ -64,9 +64,12 @@ function collectData(e) {
     }
     switch(data) {
         case '0':
-            if (operatorIsActive == false && value1 != 0){
+            if (operatorIsActive == false && (value1 != 0 || value1.includes('.') == true)){
                 value1 = value1 + '0';
-            } 
+            } else if (value1 == 0 || operatorIsActive == true) {
+                value1 = '0'
+                operatorIsActive = false;
+            }
             break;
         case '9':
             if (operatorIsActive == false && (value1 != 0 || value1.includes('.') == true)){
@@ -150,15 +153,14 @@ function collectData(e) {
             break;
         case 'clear':
             value1 = '0';
+            document.getElementById("superior").textContent = "\xa0";
             break;
         case 'pi':
             value1 = '3.141592653589793';
+            operatorIsActive = true;
             break;            
         case 'plusminus':
             value1 = plusminus(value1);
-            if (value1==0){
-                value1=''
-            }
             operatorIsActive = true;
             break;
         case 'squareroot':
@@ -168,9 +170,9 @@ function collectData(e) {
             operatorIsActive = true;
             break;
         case 'power':
-            if (value1 != ""){
-                value1 = squareroot(value1);
-            }
+            value2 = value1;
+            operator = "power"
+            document.getElementById("superior").textContent = value2+" ^";
             operatorIsActive = true;
             break;
         case 'factorial':
@@ -189,10 +191,58 @@ function collectData(e) {
             }
             operatorIsActive = true;
             break;
+        case 'sum':
+        case '+':
+            value2 = value1;
+            operator = "sum"
+            document.getElementById("superior").textContent = value2+" +";
+            operatorIsActive = true;
+            break;
+        case 'subtract':
+        case '-':
+            value2 = value1;
+            operator = "subtract"
+            document.getElementById("superior").textContent = value2+" -";
+            operatorIsActive = true;
+            break;
+        case 'multiply':
+        case '*':
+            value2 = value1;
+            operator = "multiply"
+            document.getElementById("superior").textContent = value2+" *";
+            operatorIsActive = true;
+            break;
+        case 'division':
+        case '/':
+            value2 = value1;
+            operator = "division"
+            document.getElementById("superior").textContent = value2+" /";
+            operatorIsActive = true;
+            break;
+        case 'equal':
+        case 'Enter':
+            value1 = equal();
+            value2 = '';
+            document.getElementById("superior").textContent = "\xa0";
+            operatorIsActive = true;
         
     }
     document.getElementById("lastinput").textContent = value1;
     
 }
 
-
+function equal() {
+    switch(operator) {
+        case 'sum':
+            return Number(value2)+Number(value1);
+        case 'subtract':
+            return Number(value2)-Number(value1);
+        case 'multiply':
+            return value2*value1;
+        case 'division':
+            return value2/value1;
+        case 'power':
+            return value2**value1;
+        
+    }
+}
